a6a079ce966c8d508d6ad2c5cb4f88d26d4100f5
Yurii Rysyn <zerocool_cs@ukr.net>
2021-03-07 13:28:23 +0200

    created App and Header, start working on burger menu and nav for mobile

---
+++ src/components/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Header from './Header/Header';
+
+import './App.scss';
+import '../styles/_utils.scss';
+
+const cssClasses = 'app app--active';
+// const styleObj = {
+//   backgroundColor: 'yellow',
+// }
+
+
+function App () {
+  return (
+      <Header 
+        pathname={'dadada'}
+      />
+
+  );
+ }
+
+export default App;

026b0d718df1c58412579b321dd649f4a2988325
Yurii Rysyn <zerocool_cs@ukr.net>
2021-03-14 14:36:27 +0200

    Updates

---
+++ precache-manifest.63bd60768dcfbd697ef9638d1772f371.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8001b78c07008617a206a26b13cc102",
+    "url": "/store/index.html"
+  },
+  {
+    "revision": "59ed8fe8eb4d6fb4e4e7",
+    "url": "/store/static/css/main.8e9e5ac2.chunk.css"
+  },
+  {
+    "revision": "489ceb2fda0a851fa650",
+    "url": "/store/static/js/2.7ff2be30.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/store/static/js/2.7ff2be30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59ed8fe8eb4d6fb4e4e7",
+    "url": "/store/static/js/main.ae060914.chunk.js"
+  },
+  {
+    "revision": "baa40557d47c32a07c05",
+    "url": "/store/static/js/runtime-main.2508a05c.js"
+  },
+  {
+    "revision": "cc148bc258b2db7752b5b5ba91d18e58",
+    "url": "/store/static/media/Favourites-Heart-Like.cc148bc2.svg"
+  },
+  {
+    "revision": "49f665877b6b6081e14cef4569870268",
+    "url": "/store/static/media/LOGO.49f66587.svg"
+  },
+  {
+    "revision": "0a64740c61b8aa13ea5e2dd0eb4726e1",
+    "url": "/store/static/media/SharpSansNo2Bold.0a64740c.otf"
+  },
+  {
+    "revision": "20b773328c98c9f9c7ce59c551f05516",
+    "url": "/store/static/media/SharpSansNo2Medium.20b77332.otf"
+  },
+  {
+    "revision": "c90f75b930746d6bc66e82baf982ca34",
+    "url": "/store/static/media/SharpSansNo2Semibold.c90f75b9.otf"
+  },
+  {
+    "revision": "032a50cf1345af9ac76324ff5b0e4e2d",
+    "url": "/store/static/media/basket.032a50cf.svg"
+  }
+]);
